3b619448ae15a76948a4074308abdcbf1fdf7639
Yaroslav Kazaryan <aroslavkazaryan@gmail.com>
2025-06-20 21:13:12 +0300

    tyda

--- main.c
+++ main.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
 
+#include "askname.h"
+
 int main(int argc, char **argv)
 {
-    printf("Hello world!\n");
+    char first[255], last[255];
+
+    askname(first, last);
+
+    printf("Hello, %s %s!\n", first, last);
     return 0;
 }
